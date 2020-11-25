6881e15836aa07436fcf6a8733f79b6995d3b0fc
braedennz <braedenltd@gmail.com>
2020-11-25 23:14:02 +1300

    added docker base

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -3,59 +3,55 @@ import React, { useEffect } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
-import Sharedb from 'sharedb/lib/client';
-import richText from 'rich-text';
+import { io } from 'socket.io-client';
 
 function Editor() {
-    // Registering the rich text type to make sharedb work
-    // with our quill editor
-    Sharedb.types.register(richText.type);
-
-    // Connecting to our socket server
-    const socket = new WebSocket('ws://127.0.0.1:8080');
-    const connection = new Sharedb.Connection(socket);
-
-    // Querying for our document
-    const doc = connection.get('documents', 'firstDocument');
-
     useEffect(() => {
-        doc.subscribe(function (err) {
-            if (err) throw err;
-
-            const options = {
-                theme: 'snow',
-                modules: {
-                toolbar: '#toolbar'
-                },
-            };
-            let quill = new Quill('#editor', options);
-            /**
-             * On Initialising if data is present in server
-             * Updaing its content to editor
-             */
-            quill.setContents(doc.data);
-
-            /**
-             * On Text change publishing to our server
-             * so that it can be broadcasted to all other clients
-             */
-            quill.on('text-change', function (delta, oldDelta, source) {
-                if (source !== 'user') return;
-                doc.submitOp(delta, { source: quill });
-            });
-
-            /** listening to changes in the document
-             * that is coming from our server
-             */
-            doc.on('op', function (op, source) {
-                if (source === quill) return;
-                quill.updateContents(op);
-            });
+        const socket = io('localhost:4000', {
+            transports: ['websocket', 'polling', 'flashsocket']
         });
 
-        return () => {
-            connection.close();
+        const options = {
+            theme: 'snow',
+            modules: {
+                toolbar: '#toolbar'
+            },
         };
+
+        let quill = new Quill('#editor', options);
+
+        /**
+         * On Initialising if data is present in server
+         * Updaing its content to editor
+         */
+        socket.on('set-text', (data) => {
+            quill.setContents(JSON.parse(data));
+        });
+
+        /** listening to changes in the document
+         * that is coming from our server
+         */
+        socket.on('updated-text', (data) => {
+            if (!data) {
+                return;
+            }
+
+            quill.updateContents(JSON.parse(data));
+        });
+
+        /**
+         * On Text change publishing to our server
+         * so that it can be broadcasted to all other clients
+         */
+        quill.on('text-change', function (delta, oldDelta, source) {
+            if (source !== 'user') return;
+
+            let text = quill.getContents().ops;
+
+            socket.emit('update-text', text);
+        });
+
+        return () => socket.disconnect();
     }, []);
     
     return (
