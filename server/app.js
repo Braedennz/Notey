558425077d47f6491851021a13d7fc9d30add1d3
braedennz <braedenltd@gmail.com>
2020-11-25 22:35:49 +1300

    continued work

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 var http = require('http').createServer(app);
 
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, { origins: '*:*'});
 
 const routes = require('./routes');
 app.use('/api', routes)
@@ -15,6 +15,21 @@ http.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
 });
 
+let textData = null;
+
 io.on('connection', (socket) => { 
     console.log('new client connected');
+
+    if (textData) {
+        socket.emit('set-text', textData);
+    }
+
+    socket.on('update-text', data => {
+        textData = JSON.stringify(data);
+        //socket.emit('updated-text', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log("Client disconnected");
+    });
 });
